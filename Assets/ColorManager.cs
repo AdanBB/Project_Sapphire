a306b6af4e3c213d18f5d9486f81a8e525a31d81
Xiskuu <xiisku@gmail.com>
2016-04-25 01:41:22 +0200

    ColorManager

---
+++ Assets/ColorManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColorManager : MonoBehaviour {
+	
+	public List<Color> colors;
+	public List<Color> colorsUnlock;
+
+	public bool[] boolsColors;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void adColor(int idColor){
+	
+		if (idColor == 0 && !boolsColors[0]) {
+		
+		
+			colorsUnlock.Add (colors [0]);
+			boolsColors [0] = !boolsColors [0];
+
+		
+		}
+		if (idColor == 1) {
+
+
+			colorsUnlock.Add (colors [1]);
+
+
+		}
+	
+	}
+}
--- Assets/Scripts/TintColor.cs
+++ Assets/Scripts/TintColor.cs
@@ -5,6 +5,9 @@ public class TintColor : MonoBehaviour {
     public GameObject bullet;
 
 	public AudioClip getColor;
+
+	public int idColor;
+	public ColorManager colorManager;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,17 +15,19 @@ public class TintColor : MonoBehaviour {
 
     void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
         {
-            bullet.GetComponent<Renderer>().sharedMaterial.color = this.GetComponent<Renderer>().sharedMaterial.GetColor("_Color");
+            //bullet.GetComponent<Renderer>().sharedMaterial.color = this.GetComponent<Renderer>().sharedMaterial.GetColor("_Color");
 
 			Invoke ("colorSet", 0.1f);
+
+			colorManager.adColor (idColor);
         }
     }
 
 	void colorSet(){
 	
+
 		gameObject.GetComponent<AudioSource> ().PlayOneShot (getColor);
-	
 	}
 }
