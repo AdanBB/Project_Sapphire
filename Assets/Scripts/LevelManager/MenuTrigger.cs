3626e6f1c516a08c16b744c0de7afb2f2a296e3c
AdanBB <adanbaro2014@cev.com>
2016-04-26 13:59:58 +0200

    Menu Level

---
+++ Assets/Scripts/LevelManager/MenuTrigger.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuTrigger : MonoBehaviour {
+
+	public string scene;
+	public Image black;
+	public Image level;
+	private float count;
+	private bool faderActive;
+	private bool endActive;
+
+	void Awake()
+	{
+		faderActive = false;
+		endActive = false;
+	}
+
+	void Update()
+	{
+		if (faderActive == true)
+		{
+			EndFade(level);
+		}
+		if (endActive == true) 
+		{
+			EndFade(black);
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			
+			faderActive = true;
+
+			if (Input.GetKey (KeyCode.E)) 
+			{
+				endActive = true;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			faderActive = false;
+			StartFade (level);
+		}
+	}
+
+	public void EndFade(Image toFade)
+	{
+		count = count + (1f/120f);
+		toFade.color = new Color(toFade.color.r, toFade.color.g, toFade.color.b, count);
+
+		if (toFade.color.a >= 0.95f)
+		{
+			SceneManager.LoadScene(scene);
+		}
+	}
+
+	public void StartFade(Image toFade)
+	{
+		count = count + (1f/120f);
+		toFade.color = new Color(toFade.color.r, toFade.color.g, toFade.color.b, count);
+
+		if (toFade.color.a >= 0.95f)
+		{
+			SceneManager.LoadScene(scene);
+		}
+	}
+}
