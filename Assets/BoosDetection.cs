fdcd4a88b3848df06301bdcf3ccf6d06a043be69
Fran <xiisku@gmail.com>
2016-04-26 23:11:40 +0200

    Enemy_X Building

---
+++ Assets/BoosDetection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoosDetection : MonoBehaviour {
+
+	private Animator boosAnimator;
+	// Use this for initialization
+	void Start () {
+	
+
+		boosAnimator = GetComponentInChildren<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+
+		if (other.tag == "Player") {
+		
+		
+			boosAnimator.SetBool ("isBuilding", true);
+		
+		}
+			
+	} 
+}
