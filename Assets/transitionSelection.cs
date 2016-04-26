50c9a60b3543deb9003e36573cc30faf3a2b0d3e
AdanBB <adanbaro2014@cev.com>
2016-04-27 00:09:00 +0200

    Menu, Logo, and TransitionScene Completed

---
+++ Assets/transitionSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class transitionSelection : MonoBehaviour {
+
+    public List<GameObject> transitionList;
+    private int selected;
+
+
+	// Use this for initialization
+	void Start () {
+
+        selected = Random.Range(0, transitionList.Count);
+
+        transitionList[selected].SetActive(true);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
