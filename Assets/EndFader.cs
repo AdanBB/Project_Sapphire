6e4dfdfd7c4a322d389e6c04c1e48bfa045c9905
AdanBB <adanbaro2014@cev.com>
2016-04-27 15:38:20 +0200

    Sequencia de escenes casi acabada, transicións inicials fetes

---
+++ Assets/EndFader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndFader : MonoBehaviour
+{
+    public float timeToFade;
+    private float contador;
+    public string scene;
+    public Image fader;
+    private float count;
+
+    void Awake()
+    {
+        contador = 0;
+    }
+
+    void Update()
+    {
+        contador += Time.deltaTime;
+
+        if (contador > timeToFade)
+        {
+            EndFade();
+        }
+    }
+
+    public void EndFade()
+    {
+        count = count + (1f / 120f);
+        fader.color = new Color(fader.color.r, fader.color.g, fader.color.b, count);
+
+        if (fader.color.a >= 0.95f)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
