daa936694389e6450f3058aed7fa4064392df927
AdanBB <adanbaro2014@cev.com>
2016-04-27 17:14:27 +0200

    cosas

---
+++ Assets/Healer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+            other.GetComponent<CharacterStats>().currentHealth += other.GetComponent<CharacterStats>().maxHealth / 25;
+        }
+    }
+}
