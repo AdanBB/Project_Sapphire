814afcb0acc767823c4953b85bb7917dcc522be4
AdanBB <adanbaro2014@cev.com>
2016-05-25 19:03:09 +0200

    Fuentes añadidas
    Cambiar el ID color que tienen como variable publica para seleccionar su
    color.

---
+++ Assets/paintFontPaint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paintFontPaint : MonoBehaviour {
+
+    public GameObject font;
+
+	// Use this for initialization
+	void Start () {
+
+        if (font.GetComponent<TintColor>().idColor == 1)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (font.GetComponent<TintColor>().idColor == 0)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (GetComponent<ParticleSystem>() != null)
+        {
+            if (font.GetComponent<TintColor>().idColor == 1)
+            {
+                gameObject.GetComponent<ParticleSystem>().startColor = Color.green;
+            }
+
+            if (font.GetComponent<TintColor>().idColor == 0)
+            {
+                gameObject.GetComponent<ParticleSystem>().startColor = Color.blue;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
