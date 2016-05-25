8b0d6e12fc80ee12ea0ecb4a6569e504f0e9011c
Xiskuu <xiisku@gmail.com>
2016-05-25 21:23:22 +0200

    lvl2 m,echanicks

---
+++ Assets/blockCrack.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockCrack : MonoBehaviour {
+
+	public Animator anim;
+
+
+	private int counet;
+
+	private bool down = false;
+	private bool up = false;
+
+	public Transform A;
+
+
+	void FixedUpdate(){
+
+		if (down ) {
+		
+		
+			transform.Translate(0,0 ,20f* Time.deltaTime) ;
+
+		
+		}
+		if (transform.position.y <= -50f || up) {
+			up = true;
+			down = false;
+			transform.Translate(0,0 ,-20f* Time.deltaTime) ;
+			anim.SetBool ("IsGrow", false);
+		
+		}
+		if (transform.position.y >= -0.72f) {
+			up = false;
+			down = false;
+		
+		}
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.tag == "Player") {
+
+
+			Debug.Log ("dsadsa");
+			anim.SetBool ("IsGrow", true);
+
+			Invoke ("Down",1.3f);
+		}
+
+
+	}
+	void Down(){
+	
+		down = true;
+	}
+
+}
