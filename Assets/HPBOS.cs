73e2941b78c90c1312e55ccf7e754dc58b64a572
Fran <xiisku@gmail.com>
2016-06-03 11:45:25 +0200

    HPSliderBoos

---
+++ Assets/HPBOS.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HPBOS : MonoBehaviour {
+
+	public EnemyAiBoos hp1;
+	public EnemyAiBos2 hp2;
+	public Text text;
+	public Slider hpSliderBos;
+	// Use this for initialization
+	void Start () {
+
+		hpSliderBos.maxValue = hp1.health + hp2.health;
+		hpSliderBos.value = hp1.health + hp2.health;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		hpSliderBos.value = hp1.health + hp2.health;
+		//text.text = hp.currentHealth.ToString()+"/"+hp.maxHealth.ToString();
+	}
+}
