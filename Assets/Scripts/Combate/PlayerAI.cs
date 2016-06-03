02b23f6145bc02a5e52decc5b67821f28d8f45e8
Fran <xiisku@gmail.com>
2016-06-03 11:21:37 +0200

    Camara no se acerca tanto en Boosscene

--- Assets/Scripts/Combate/PlayerAI.cs
+++ Assets/Scripts/Combate/PlayerAI.cs
@@ -29,6 +29,7 @@ public class PlayerAI : MonoBehaviour
 	private GameObject Pivot;
 	private Vector3 pivotTransform;
 	private Vector3 _pivotTransform;
+	private Vector3 _pivotTransformBoos;
 
 	private int currentFrame;
 	private int _currentFrame;
@@ -72,6 +73,7 @@ public class PlayerAI : MonoBehaviour
 		isAttacking = false;
 		pivotTransform = new Vector3 (0.17f,2.83f,-5.08f);
 		_pivotTransform = new Vector3 (0.79f,2.42f,-2.03f);
+		_pivotTransformBoos = new Vector3 (0.36f,2.42f,-3.523f);
 		currentFrame = 0;
 		_currentFrame = 15;
 		framesDuration = 15;
@@ -251,11 +253,18 @@ public class PlayerAI : MonoBehaviour
 				//transform.localScale = new Vector3(x,y,z);
 				//transform.rotation = Quaternion.Euler(x,y,z);
 				//mat.color = new Color(r,g,b,a);
+				if(Application.loadedLevel == 6){
+					Pivot.transform.localPosition = new Vector3(Easing.QuintEaseOut(currentFrame, pivotTransform.x, (_pivotTransformBoos.x - pivotTransform.x), framesDuration),
+						Easing.QuintEaseOut(currentFrame, pivotTransform.y, (_pivotTransformBoos.y - pivotTransform.y), framesDuration),
+						Easing.QuintEaseOut(currentFrame, pivotTransform.z, (_pivotTransformBoos.z - pivotTransform.z), framesDuration));        
+					currentFrame ++;
 
+				}else{
 				Pivot.transform.localPosition = new Vector3(Easing.QuartEaseIn(currentFrame, pivotTransform.x, (_pivotTransform.x - pivotTransform.x), framesDuration),
 					Easing.QuartEaseIn(currentFrame, pivotTransform.y, (_pivotTransform.y - pivotTransform.y), framesDuration),
 					Easing.QuartEaseIn(currentFrame, pivotTransform.z, (_pivotTransform.z - pivotTransform.z), framesDuration));        
 				currentFrame++;
+				}
 			}
 		}
 
@@ -268,11 +277,18 @@ public class PlayerAI : MonoBehaviour
 				//transform.localScale = new Vector3(x,y,z);
 				//transform.rotation = Quaternion.Euler(x,y,z);
 				//mat.color = new Color(r,g,b,a);
+				if(Application.loadedLevel == 6){
+					Pivot.transform.localPosition = new Vector3(Easing.QuintEaseOut(_currentFrame, _pivotTransformBoos.x, (pivotTransform.x - _pivotTransformBoos.x), framesDuration),
+						Easing.QuintEaseOut(_currentFrame, _pivotTransformBoos.y, (pivotTransform.y - _pivotTransformBoos.y), framesDuration),
+						Easing.QuintEaseOut(_currentFrame, _pivotTransformBoos.z, (pivotTransform.z - _pivotTransformBoos.z), framesDuration));        
+					_currentFrame++;
 
+				}else {
 				Pivot.transform.localPosition = new Vector3(Easing.QuartEaseOut(_currentFrame, _pivotTransform.x, (pivotTransform.x - _pivotTransform.x), framesDuration),
 					Easing.QuartEaseOut(_currentFrame, _pivotTransform.y, (pivotTransform.y - _pivotTransform.y), framesDuration),
 					Easing.QuartEaseOut(_currentFrame, _pivotTransform.z, (pivotTransform.z - _pivotTransform.z), framesDuration));        
 				_currentFrame++;
+				}
 			}
 		}
 
